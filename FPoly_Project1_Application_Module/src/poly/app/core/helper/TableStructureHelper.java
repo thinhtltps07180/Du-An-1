05cb3daa89ce978e6980ea24d9e6847066732609
Võ Thành Tài <taivtse@gmail.com>
2018-11-03 14:12:22 +0700

    cập nhật giao diện frame nhan viên

--- FPoly_Project1_Application_Module/src/poly/app/core/helper/TableStructureHelper.java
+++ FPoly_Project1_Application_Module/src/poly/app/core/helper/TableStructureHelper.java
@@ -1,5 +1,6 @@
 package poly.app.core.helper;
 
 public class TableStructureHelper {
-    public static final String[] NGUOIDUNG_TABLE_IDENTIFIERS = {"Mã người dùng", "Họ và tên", "Số CMND", "Số điện thoại", "Email", "Địa chỉ", "Ngày vào làm", "Giới tinh", "Vai trò"};
+    public static final String[] NGUOIDUNG_TABLE_IDENTIFIERS = {"Mã người dùng", "Họ và tên", "Số CMND", "Số điện thoại", "Email", "Địa chỉ", "Ngày vào làm", "Giới tính", "Vai trò"};
+    public static final String[] KHACHHANG_TABLE_IDENTIFIERS = {"Mã khách hàng", "Họ và tên", "Số CMND", "Số điện thoại", "Email", "Địa chỉ", "Ngày đăng ký", "Ngày sinh", "Giới tính"};
 }
--- FPoly_Project1_Application_Module/src/poly/app/ui/frames/FrameQLNhanVien.java
+++ FPoly_Project1_Application_Module/src/poly/app/ui/frames/FrameQLNhanVien.java
@@ -68,10 +68,10 @@ public class FrameQLNhanVien extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(22, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(45, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -136,7 +136,7 @@ public class FrameQLNhanVien extends javax.swing.JFrame {
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 718, Short.MAX_VALUE)
                 .addGap(10, 10, 10))
         );
         jPanel5Layout.setVerticalGroup(
@@ -185,7 +185,7 @@ public class FrameQLNhanVien extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnCollapse, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
